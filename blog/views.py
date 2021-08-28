ac2c301d6d1d109dd745a0d64549dd4a16e1a055
SirkisMatt <sirkis.matt@gmail.com>
2021-08-28 19:36:15 +1200

    added some models and configured admin

--- blog/views.py
+++ blog/views.py
@@ -5,60 +5,60 @@ from django.shortcuts import render
 # Create your views here.
 
 all_posts = [
-    {
-        "slug": "hike-in-the-mountains",
-        "image": "mountains.jpg",
-        "author": "Matt Sirkis",
-        "date": date(2021, 8, 26),
-        "title": "Mountain Hiking",
-        "excerpt": "I can't wait to get back to the mountains..",
-        "content": """Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-                aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-                velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-                """
-    },
-    {
-        "slug": "programming-is-fun",
-        "image": "code.png",
-        "author": "Matt Sirkis",
-        "date": date(2022, 3, 10),
-        "title": "Programming Is Great!",
-        "excerpt": "Did you ever spend hours searching that one error in your code? Yep - that's what happened to me yesterday...",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+    # {
+    #     "slug": "hike-in-the-mountains",
+    #     "image": "mountains.jpg",
+    #     "author": "Matt Sirkis",
+    #     "date": date(2021, 8, 26),
+    #     "title": "Mountain Hiking",
+    #     "excerpt": "I can't wait to get back to the mountains..",
+    #     "content": """Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+    #             aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+    #             velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+    #             """
+    # },
+    # {
+    #     "slug": "programming-is-fun",
+    #     "image": "code.png",
+    #     "author": "Matt Sirkis",
+    #     "date": date(2022, 3, 10),
+    #     "title": "Programming Is Great!",
+    #     "excerpt": "Did you ever spend hours searching that one error in your code? Yep - that's what happened to me yesterday...",
+    #     "content": """
+    #       Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+    #       aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+    #       velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
 
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+    #       Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+    #       aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+    #       velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
 
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
-    },
-    {
-        "slug": "into-the-woods",
-        "image": "woods.jpg",
-        "author": "Matt Sirkis",
-        "date": date(2020, 8, 5),
-        "title": "Nature At Its Best",
-        "excerpt": "Nature is amazing! The amount of inspiration I get when walking in nature is incredible!",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+    #       Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+    #       aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+    #       velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+    #     """
+    # },
+    # {
+    #     "slug": "into-the-woods",
+    #     "image": "woods.jpg",
+    #     "author": "Matt Sirkis",
+    #     "date": date(2020, 8, 5),
+    #     "title": "Into The Woods",
+    #     "excerpt": "Nature is amazing! The amount of inspiration I get when walking in nature is incredible!",
+    #     "content": """
+    #       Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+    #       aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+    #       velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
 
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+    #       Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+    #       aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+    #       velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
 
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
-    }
+    #       Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+    #       aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+    #       velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+    #     """
+    # }
 ]
 
 def get_date(post):
