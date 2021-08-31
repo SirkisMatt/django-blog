609872eb3829e4d70ebc727d91c2d0be7cdd5e0c
SirkisMatt <sirkis.matt@gmail.com>
2021-08-31 19:22:33 +1200

    added comments functionality and some styling

--- blog/views.py
+++ blog/views.py
@@ -1,30 +1,60 @@
-from datetime import date
-
 from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.views.generic import ListView
+from django.views import View
+
+
 from .models import Post
+from .forms import CommentForm
 
 # Create your views here.
 
+class StartingPageView(ListView):
+    template_name = "blog/index.html"
+    model = Post
+    ordering = ["-date"]
+    context_object_name = "posts"
 
-def starting_page(request):
-    latest_posts = Post.objects.all().order_by("-date")[:3]
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        data = queryset[:3]
+        return data
 
-    return render(request, "blog/index.html", {
-        "posts": latest_posts
-    })
 
-def posts(request):
-    all_posts = Post.objects.all().order_by("-date")
+class AllPostView(ListView):
+    template_name = "blog/all-posts.html"
+    model = Post
+    ordering = ["-date"]
+    context_object_name = "all_posts"
 
-    return render(request, "blog/all-posts.html", {
-        "all_posts": all_posts
-    })
 
-def post_details(request, slug):
+class SinglePostView(View):
+    def get(self, request, slug):
+        post = Post.objects.get(slug=slug)
+        context = {
+            "post": post,
+            "post_tags": post.tags.all(),
+            "comment_form": CommentForm(),
+            "comments": post.comments.all().order_by("-id")
+        }
+        return render(request, "blog/post-details.html", context)
 
-    identified_post = get_object_or_404(Post, slug=slug)
+    def post(self, request, slug):
+        post = Post.objects.get(slug=slug)
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return HttpResponseRedirect(reverse("post-details-page", args=[slug]))
 
-    return render(request, "blog/post-details.html", {
-        "post": identified_post,
-        "post_tags": identified_post.tags.all()
-    })
+
+        context = {
+            "post": post,
+            "post_tags": post.tags.all(),
+            "comment_form": CommentForm(),
+            "comments": post.comments.all().order_by("-id")
+        }
+        return render(request, "blog/post-details.html", context)
+    
